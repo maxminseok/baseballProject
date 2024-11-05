9131f6a29fb2a03d3186b55e4683aa9678b594a3
minseok <pms950106@gmail.com>
2024-11-05 15:26:31 +0900

    [FIX] 처음 번호 입력시1~3이 아니면 한번만 입력받고 끝나는 오류 수정

--- BaseballGameProject/BaseballGameProject/main.swift
+++ BaseballGameProject/BaseballGameProject/main.swift
@@ -92,70 +92,74 @@ print("환영합니다! 원하시는 번호를 입력해주세요")
 print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
 var gameSelect = readLine()!
 var gameCount: [Int] = [] // 게임 카운트, index번째 게임의 시도횟수 tryCount를 추가하기 위한 배열
-if gameSelect != "1" && gameSelect != "2" && gameSelect != "3" { print ("1~3 사이의 숫자를 입력해주세요")} // 입력이 1,2,3 아닐 경우 메세지 출력, 1~3 아닌 숫자일 때 재입력 코드 필요
-else {
-    while gameSelect != "3" { // 3일 경우 게임 종료, 1,2일 경우 아래 코드 동작
-        var tryCount = 0 // 시도 횟수 카운트
-        if gameSelect == "1" { // 1일 경우 게임 시작
-            print("게임을 시작합니다")
-            print("숫자를 입력하세요")
-            
-            var answer = makeAnswer().randomAnswer() // 정답 숫자
-            let convert = convertNumber() // 배열로 변환할 인스턴스
-            let CheckZero = checkZero() // 0 있는지 체크할 인스턴스
-            let CheckDup = checkDuplication() // 중복 체크할 인스턴스
-            let CheckStrike = checkStrike() // 스트라이크를 체크할 인스턴스
-            let CheckCorrectInput = checkCorrectInput() // 입력이 맞게 되었는지 확인할 인스턴스
-            
-            var answerArray = convert.convertArray(input: answer) // 정답 숫자를 문자로 변환 후 배열에 저장
-            while CheckZero.check(input: answerArray) || CheckDup.check(input: answerArray) { // 정답 글자의 중복 여부와 첫글자 0 유무 확인, 둘 중 하나라도 있으면 정답 재설정
-                answer = makeAnswer().randomAnswer()
-                answerArray = convert.convertArray(input: answer)
-            }
-            
-            var inputNumber = readLine()!
-            while CheckCorrectInput.check(input: inputNumber) { // 입력에 이상 없는지 체크
-                inputNumber = CheckCorrectInput.repeatInput() // 이상 있으면 없을때까지 재입력
-            }
-            
-            var inputArray = convert.convertArray(input: inputNumber) // 비교를 위해 문자로 바꿔 배열에 저장
-            var checkStrikeCount = CheckStrike.check(answerArray: answerArray, inputArray: inputArray) // 볼&스트라이크 판별
-            print("\(checkStrikeCount.strike)스트라이크 \(checkStrikeCount.ball)볼")
-            
-            while checkStrikeCount.strike != 3 { // 스트라이크가 3개가 아니면 계속 반복
-                print("\n숫자를 입력하세요")
-                inputNumber = readLine()! // 숫자 재입력
-                while CheckCorrectInput.check(input: inputNumber) { // 다시 입력한 숫자에 문제 없는지 체크
-                    inputNumber = CheckCorrectInput.repeatInput() // 다시 입력한 숫자가 문제 없을떄까지 재입력
-                }
-                inputArray = convert.convertArray(input: inputNumber) // 재비교를 위해 다시 배열에 저장
-                checkStrikeCount = CheckStrike.check(answerArray: answerArray, inputArray: inputArray) // 다시 스트라이크 체크
-                print("\(checkStrikeCount.strike)스트라이크 \(checkStrikeCount.ball)볼")
-                tryCount += 1 // 시도 횟수 증가
-            }
-            
-            gameCount.append(tryCount) // 정답 맞춘 후 배열에 시도 횟수 추가
-            print("정답입니다!")
-            print("\n원하시는 번호를 입력해주세요")
-            print("1. 게임 재시작하기  2. 게임 기록 보기  3. 종료하기")
-            gameSelect = readLine()!
-        }
-        if gameSelect == "2" && !gameCount.isEmpty { // 기록 출력, 게임 기록이 있으면서 2가 눌렸을 때 동작
-            for i in 1...gameCount.count {
-                print("\(i)번째 게임 : 시도 횟수 - \(gameCount[i-1])") // 배열에 저장된 시도 횟수 출력
-            }
-            print("\n원하시는 번호를 입력해주세요")
-            print("1. 게임 재시작하기  2. 게임 기록 보기  3. 종료하기")
-            gameSelect = readLine()!
-        } else if gameSelect == "2" && gameCount.isEmpty { // 2번 눌렀는데 게임 기록이 없을때 동작
-            print("아직 게임 기록이 없습니다")
-            print("1. 게임 재시작하기  또는  3. 종료하기 를 입력해주세요")
-            gameSelect = readLine()!
-        } else if gameSelect != "1" && gameSelect != "2" && gameSelect != "3" { // 게임 기록 없을 때 1,2,3 이외의 입력이 들어올 때 동작
-            print ("1~3 사이의 숫자를 입력해주세요")
-            gameSelect = readLine()!
-        }
-    }
-    print("숫자 야구 게임을 종료합니다")
+
+while gameSelect != "1" && gameSelect != "2" && gameSelect != "3" { // 입력이 1,2,3 아닐 경우 메세지 출력
+    print ("1~3 사이의 숫자를 입력해주세요")
+    gameSelect = readLine()!
 }
+
+while gameSelect != "3" { // 3일 경우 게임 종료, 1,2일 경우 아래 코드 동작
+    var tryCount = 0 // 시도 횟수 카운트
+    if gameSelect == "1" { // 1일 경우 게임 시작
+        print("게임을 시작합니다")
+        print("숫자를 입력하세요")
+        
+        var answer = makeAnswer().randomAnswer() // 정답 숫자
+        let convert = convertNumber() // 배열로 변환할 인스턴스
+        let CheckZero = checkZero() // 0 있는지 체크할 인스턴스
+        let CheckDup = checkDuplication() // 중복 체크할 인스턴스
+        let CheckStrike = checkStrike() // 스트라이크를 체크할 인스턴스
+        let CheckCorrectInput = checkCorrectInput() // 입력이 맞게 되었는지 확인할 인스턴스
+        
+        var answerArray = convert.convertArray(input: answer) // 정답 숫자를 문자로 변환 후 배열에 저장
+        while CheckZero.check(input: answerArray) || CheckDup.check(input: answerArray) { // 정답 글자의 중복 여부와 첫글자 0 유무 확인, 둘 중 하나라도 있으면 정답 재설정
+            answer = makeAnswer().randomAnswer()
+            answerArray = convert.convertArray(input: answer)
+        }
+        
+        var inputNumber = readLine()!
+        while CheckCorrectInput.check(input: inputNumber) { // 입력에 이상 없는지 체크
+            inputNumber = CheckCorrectInput.repeatInput() // 이상 있으면 없을때까지 재입력
+        }
+        
+        var inputArray = convert.convertArray(input: inputNumber) // 비교를 위해 문자로 바꿔 배열에 저장
+        var checkStrikeCount = CheckStrike.check(answerArray: answerArray, inputArray: inputArray) // 볼&스트라이크 판별
+        print("\(checkStrikeCount.strike)스트라이크 \(checkStrikeCount.ball)볼")
+        
+        while checkStrikeCount.strike != 3 { // 스트라이크가 3개가 아니면 계속 반복
+            print("\n숫자를 입력하세요")
+            inputNumber = readLine()! // 숫자 재입력
+            while CheckCorrectInput.check(input: inputNumber) { // 다시 입력한 숫자에 문제 없는지 체크
+                inputNumber = CheckCorrectInput.repeatInput() // 다시 입력한 숫자가 문제 없을떄까지 재입력
+            }
+            inputArray = convert.convertArray(input: inputNumber) // 재비교를 위해 다시 배열에 저장
+            checkStrikeCount = CheckStrike.check(answerArray: answerArray, inputArray: inputArray) // 다시 스트라이크 체크
+            print("\(checkStrikeCount.strike)스트라이크 \(checkStrikeCount.ball)볼")
+            tryCount += 1 // 시도 횟수 증가
+        }
+        
+        gameCount.append(tryCount) // 정답 맞춘 후 배열에 시도 횟수 추가
+        print("정답입니다!")
+        print("\n원하시는 번호를 입력해주세요")
+        print("1. 게임 재시작하기  2. 게임 기록 보기  3. 종료하기")
+        gameSelect = readLine()!
+    }
+    
+    if gameSelect == "2" && !gameCount.isEmpty { // 기록 출력, 게임 기록이 있으면서 2가 눌렸을 때 동작
+        for i in 1...gameCount.count {
+            print("\(i)번째 게임 : 시도 횟수 - \(gameCount[i-1])") // 배열에 저장된 시도 횟수 출력
+        }
+        print("\n원하시는 번호를 입력해주세요")
+        print("1. 게임 재시작하기  2. 게임 기록 보기  3. 종료하기")
+        gameSelect = readLine()!
+    } else if gameSelect == "2" && gameCount.isEmpty { // 2번 눌렀는데 게임 기록이 없을때 동작
+        print("아직 게임 기록이 없습니다")
+        print("1. 게임 재시작하기  또는  3. 종료하기 를 입력해주세요")
+        gameSelect = readLine()!
+    } else if gameSelect != "1" && gameSelect != "2" && gameSelect != "3" { // 게임 기록 없을 때 1,2,3 이외의 입력이 들어올 때 동작
+        print ("1~3 사이의 숫자를 입력해주세요")
+        gameSelect = readLine()!
+    }
+}
+print("숫자 야구 게임을 종료합니다")
 
