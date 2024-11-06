4479e2c7ef3c1f62f7ce65b32286ee4fd9838de5
minseok <pms950106@gmail.com>
2024-11-06 21:11:43 +0900

    [COMMENT] 주석 변경

--- BaseballGameProject/Main/main.swift
+++ BaseballGameProject/Main/main.swift
@@ -10,11 +10,9 @@ import Foundation
 print("환영합니다! 원하시는 번호를 입력해주세요")
 print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
 
-/*           게임 선택에 대한 입력 받기           */
 var gameSelect = readLine()!
 var gameCount: [Int] = [] // 게임 카운트, index번째 게임의 시도횟수 tryCount를 추가하기 위한 배열
 
-/*           게임 선택에 대한 입력이 옳은지 체크            */
 while gameSelect != "1" && gameSelect != "2" && gameSelect != "3" { // 입력이 1,2,3 아닐 경우 메세지 출력
     print ("1~3 사이의 숫자를 입력해주세요")
     gameSelect = readLine()!
